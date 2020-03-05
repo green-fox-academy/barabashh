7cf60528de004b3c9c8a99e36efb20e3db21d436
barabashh <60752312+barabashh@users.noreply.github.com>
2020-03-05 12:30:11 +0000

    solved first 3 exercises

---
+++ week-01/day-04/src/HelloOthers.java
@@ -0,0 +1,13 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello, Kata");
+        System.out.println("Hello, Barna");
+        System.out.println("Hello, Ahmed");
+        // Greet 3 of your classmates with this program, in three separate lines
+        // like:
+        //
+        // Hello, Esther!
+        // Hello, Mary!
+        // Hello, Joe!
+    }
+}
