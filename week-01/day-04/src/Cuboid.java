8cfc6b4cf13fdf6a0782b5c8c905349b2c3fc566
barabashh <60752312+barabashh@users.noreply.github.com>
2020-03-05 14:41:20 +0000

    exercises done for Variables

---
+++ week-01/day-04/src/Cuboid.java
@@ -0,0 +1,9 @@
+public class Cuboid {
+    public static void main(String[] args) {
+        double l = 2;
+        double w = 3.;
+        double h = 4;
+        System.out.println("Surface Area: " + 2 * (l * w + l * h + h * w));
+        System.out.println("Volume: " + l * w * h);
+    }
+}
---
+++ week-01/day-04/src/DefineBasicInfo.java
@@ -0,0 +1,17 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        // Define several things as a variable then print their values
+        // Your name as a string
+        String myName = "Gabor";
+        // Your age as an integer
+        int myAge = 40;
+        // Your height in meters as a double
+        double myHeight = 1.78;
+        // Whether you are married or not as a boolean
+        boolean married = true;
+        System.out.println("My name is: " + myName);
+        System.out.println("My age is: " + myAge);
+        System.out.println("My height is: " + myHeight);
+        System.out.println("I am married: " + married);
+    }
+}
