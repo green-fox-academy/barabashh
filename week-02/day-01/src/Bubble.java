ca9415b6ee0cdb98e4f54ee936f6021e0358f278
barabashh <60752312+barabashh@users.noreply.github.com>
2020-03-09 16:14:15 +0000

    Bubble solved

---
+++ week-02/day-01/src/Bubble.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Bubble {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(bubble(new int[] {34, 12, 24, 9, 5})));
+        //  should print [5, 9, 12, 24, 34]
+        System.out.println(Arrays.toString(advancedBubble(new int[] {34, 12, 24, 9, 5}, true)));
+        //  should print [34, 24, 12, 9, 5]
+    }
+    public static int[] bubble(int[] numbersToSort)
+    {
+        Arrays.sort(numbersToSort);
+        return numbersToSort;
+    }
+    public static int[] advancedBubble(int[] numbersToSort, boolean reverse)
+    {
+        if (reverse == false)
+        {
+            Arrays.sort(numbersToSort);
+            return numbersToSort;
+        }
+        else
+        {
+            Arrays.sort(numbersToSort);
+            int[] reversed = new int[numbersToSort.length];
+            for (int i = 0; i < numbersToSort.length; i++) {
+                reversed[i] = numbersToSort[numbersToSort.length - i - 1];
+            }
+            numbersToSort = Arrays.copyOf(reversed, reversed.length);
+            return numbersToSort;
+        }
+    }
+}
