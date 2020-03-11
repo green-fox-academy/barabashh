7cddb71969e51c9cb0cfb4e1fb0d6a95ce48557d
barabashh <60752312+barabashh@users.noreply.github.com>
2020-03-11 16:29:25 +0000

    doubled exercise solved

---
+++ week-02/day-03/src/Doubled.java
@@ -0,0 +1,37 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Doubled {
+    public static void main(String[] args) {
+        System.out.println(decryptFile("assets/duplicated-chars.txt", "assets/decryptedFile.txt"));
+    }
+
+    public static Boolean decryptFile(String fileToDecrypt, String writeTheDecryptedFileTo) {
+        Path fileToDecryptPath = Paths.get(fileToDecrypt);
+        Path writeTheDecryptedFileToPath = Paths.get(writeTheDecryptedFileTo);
+        List<String> contentToDeCrypt;
+        List<String> contentToWrite = new ArrayList<String>();
+        try {
+            contentToDeCrypt = Files.readAllLines(fileToDecryptPath);
+        } catch (Exception e) {
+            return false;
+        }
+
+        for (String line : contentToDeCrypt) {
+            String tempLine = "";
+            for (int i = 0; i < line.length(); i++) {
+                if (i % 2 == 0) tempLine += line.charAt(i);
+            }
+            contentToWrite.add(tempLine);
+        }
+        try {
+            Files.write(writeTheDecryptedFileToPath, contentToWrite);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+}
