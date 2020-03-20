234fdce1162e29c08b9e631224e932a69eef6f5b
barabashh <60752312+barabashh@users.noreply.github.com>
2020-03-20 14:25:26 +0000

    fibonacci improved and tested against algorithmic result too

--- week-03/day-04/src/fibonacci/Fibonacci.java
+++ week-03/day-04/src/fibonacci/Fibonacci.java
@@ -1,16 +1,16 @@
 package fibonacci;
 
 public class Fibonacci {
-  public int countFibonacciAtIndexByAddition(int index) {
+  public long countFibonacciAtIndexByAddition(int index) {
     if (index == 0) {
       return 0;
     }
     if (index == 1) {
       return 1;
     }
-    int nMinus2 = 0;
-    int nMinus1 = 1;
-    int n = 0;
+    long nMinus2 = 0;
+    long nMinus1 = 1;
+    long n = 0;
     for (int i = 0; i < index - 1; i++) {
       n = nMinus2 + nMinus1;
       //set the variables at the next place:
@@ -19,4 +19,10 @@ public class Fibonacci {
     }
     return n;
   }
+
+  public long countFibonacciAtIndexByAlgorithm(int index) {
+    double goldenRatio = (1 + Math.sqrt(5)) / 2;
+    return (long) ((Math.pow(goldenRatio, index) - Math.pow(-(1 - goldenRatio), index)) / Math.sqrt(5));
+  }
 }
+
--- week-03/day-04/test/fibonacci/FibonacciTest.java
+++ week-03/day-04/test/fibonacci/FibonacciTest.java
@@ -10,32 +10,38 @@ public class FibonacciTest {
 
   @Before
   public void setup() {
-  fibonacci = new Fibonacci();
+    fibonacci = new Fibonacci();
   }
 
   @Test
   public void shouldReturn0AtIndex0() {
-    assertEquals(0,fibonacci.countFibonacciAtIndexByAddition(0));
+    assertEquals(0, fibonacci.countFibonacciAtIndexByAddition(0));
   }
 
   @Test
   public void shouldReturn1AtIndex1() {
-    assertEquals(1,fibonacci.countFibonacciAtIndexByAddition(1));
+    assertEquals(1, fibonacci.countFibonacciAtIndexByAddition(1));
   }
 
   @Test
   public void shouldReturn1AtIndex2() {
-    assertEquals(1,fibonacci.countFibonacciAtIndexByAddition(2));
+    assertEquals(1, fibonacci.countFibonacciAtIndexByAddition(2));
   }
 
   @Test
   public void shouldReturn2AtIndex3() {
-    assertEquals(2,fibonacci.countFibonacciAtIndexByAddition(3));
+    assertEquals(2, fibonacci.countFibonacciAtIndexByAddition(3));
   }
 
   @Test
   public void shouldReturn34AtIndex9() {
-    assertEquals(34,fibonacci.countFibonacciAtIndexByAddition(9));
+    assertEquals(34, fibonacci.countFibonacciAtIndexByAddition(9));
+  }
+
+  @Test
+  public void shouldReturnTheSameAsAlgorithmicAtIndex70() {
+    assertEquals(fibonacci.countFibonacciAtIndexByAddition(70),
+        fibonacci.countFibonacciAtIndexByAlgorithm(70));
   }
 
 }
