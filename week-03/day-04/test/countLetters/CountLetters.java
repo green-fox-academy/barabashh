28a5b19df3077b4e6de45124cd4c4023c6affa39
barabashh <60752312+barabashh@users.noreply.github.com>
2020-03-20 11:02:19 +0000

    Count letters done

--- week-03/day-04/test/countLetters/CountLetters.java
+++ week-03/day-04/test/countLetters/CountLetters.java
@@ -1,7 +1,24 @@
 package countLetters;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class CountLetters {
-  public HashMap<>
+  public HashMap<Character,Integer> countLetters(String word){
+    ArrayList<Character> chars = new ArrayList<>();
+    for (char ch : word.toCharArray()) {
+      chars.add(ch);
+    }
+    HashMap<Character,Integer> lettersMap = new HashMap<>();
+    for (char ch: chars) {
+      if(lettersMap.containsKey(ch)){
+        int currentCount = lettersMap.get(ch);
+        lettersMap.put(ch, ++currentCount);
+      }else
+      {
+        lettersMap.put(ch,1);
+      }
+    }
+    return lettersMap;
+  }
 }
---
+++ week-03/day-04/test/countLetters/CountLettersTest.java
@@ -0,0 +1,49 @@
+package countLetters;
+
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CountLettersTest {
+  CountLetters countLetters;
+
+  @Before
+  public void setup() {
+    countLetters = new CountLetters();
+  }
+
+  @Test
+  public void shoudCreateMapFromSimpleWord() {
+    HashMap<Character, Integer> testResult = new HashMap<>();
+    testResult.put('A', 1);
+    testResult.put('p', 2);
+    testResult.put('l', 1);
+    testResult.put('e', 1);
+    assertEquals(testResult,countLetters.countLetters("Apple"));
+  }
+
+  @Test
+  public void shoudCreateMapFromLongerWord() {
+    HashMap<Character, Integer> testResult = new HashMap<>();
+    testResult.put('R', 1);
+    testResult.put('e', 1);
+    testResult.put('s', 2);
+    testResult.put('p', 1);
+    testResult.put('o', 1);
+    testResult.put('n', 1);
+    testResult.put('i', 3);
+    testResult.put('b', 1);
+    testResult.put('l', 1);
+    testResult.put('t', 1);
+    testResult.put('y', 1);
+    assertEquals(testResult,countLetters.countLetters("Responsibility"));
+  }
+
+  @Test
+  public void shoudReturnEmptyMapFromEmptyString() {
+    HashMap<Character, Integer> testResult = new HashMap<>();
+    assertEquals(testResult,countLetters.countLetters(""));
+  }
+}
