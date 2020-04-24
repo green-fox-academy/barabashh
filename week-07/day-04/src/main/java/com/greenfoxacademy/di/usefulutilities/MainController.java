b9ecc886c21d5a3e2ec66017c5879d794311218c
barabashh <60752312+barabashh@users.noreply.github.com>
2020-04-24 10:44:29 +0100

    Colored backround xercise completed

---
+++ week-07/day-04/src/main/java/com/greenfoxacademy/di/usefulutilities/MainController.java
@@ -0,0 +1,32 @@
+package com.greenfoxacademy.di.usefulutilities;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/useful")
+public class MainController {
+  private UtilityService utilityService;
+
+  @Autowired
+  public MainController(UtilityService utilityService) {
+    this.utilityService = utilityService;
+  }
+
+  @GetMapping("/")
+  public String Index() {
+
+    return "index";
+  }
+
+  @GetMapping("/colored")
+  public String Colored(Model model){
+    model.addAttribute("backGroundColor",utilityService.randomColor());
+    return "colored";
+  }
+
+
+}
