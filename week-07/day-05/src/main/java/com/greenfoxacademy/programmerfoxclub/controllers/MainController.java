8f066e208ffce8c7e441c0457068e896b83559bf
barabashh <60752312+barabashh@users.noreply.github.com>
2020-05-11 19:27:13 +0100

    sending data from dropdown is not working yet..

--- week-07/day-05/src/main/java/com/greenfoxacademy/programmerfoxclub/controllers/MainController.java
+++ week-07/day-05/src/main/java/com/greenfoxacademy/programmerfoxclub/controllers/MainController.java
@@ -3,11 +3,17 @@ package com.greenfoxacademy.programmerfoxclub.controllers;
 import com.greenfoxacademy.programmerfoxclub.models.Fox;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.EnumSet;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -16,13 +22,28 @@ public class MainController {
 
   public MainController() {
     foxes.add(new Fox("Mr. Fox",
-        Arrays.asList("Write HTML", "code in java"), "pizza", Fox.Drink.LEMONADE));
+        Arrays.asList("Write HTML", "code in java"), Fox.Food.PIZZA, Fox.Drink.LEMONADE));
+    foxes.add(new Fox("Jozsi",
+        Arrays.asList("Write Jozsi into HTML", "write Jozsi in java"), Fox.Food.PIZZA, Fox.Drink.LEMONADE));
     foxes.add(new Fox("Karak",
-        null, "salad", Fox.Drink.WATER));
+        null, Fox.Food.SALAD, Fox.Drink.WATER));
   }
 
-  @GetMapping(path = "/")
-  public String index() {
+  @RequestMapping(path = {"/", "/information"})
+  public String index(@RequestParam(required = false) String name, Model model) {
+    if (name == null) {
+      return "redirect:/login";
+    }
+    Optional<Fox> optionalFox = foxes.stream()
+        .filter(f -> f.getName().equals(name))
+        .findFirst();
+    if (!optionalFox.isPresent()) {
+      return "redirect:/login";
+    } else {
+      Fox fox = optionalFox.get();
+      model.addAttribute("fox", fox);
+      model.addAttribute("name", name);
+    }
     return "index";
   }
 
@@ -31,9 +52,24 @@ public class MainController {
     return "login";
   }
 
-  @PostMapping(path = "/")
-  public String postLogin(@RequestParam String name, Model model) {
+  @PostMapping(path = "/login")
+  public String postLogin(@RequestParam String name) {
+    return "redirect:/?name=" + name;
+  }
+
+  @GetMapping(path = "/nutritionStore")
+  public String nutritionStoreGet(@RequestParam(required = false) String name, Model model){
+    List<String> foodNames = Stream.of(Fox.Food.values())
+        .map(Enum::name)
+        .collect(Collectors.toList());
+    model.addAttribute("foodNames", foodNames);
     model.addAttribute("name", name);
-    return "index";
+    return "nutritionStore";
+  }
+
+  @PostMapping(path = "/nutritionStore")
+  public String nutritionStorePost(@RequestParam(required = false) String name
+      ,@RequestParam String foodChoice, Model model) {
+    return "nutritionStore";
   }
 }
--- week-07/day-05/src/main/java/com/greenfoxacademy/programmerfoxclub/models/Fox.java
+++ week-07/day-05/src/main/java/com/greenfoxacademy/programmerfoxclub/models/Fox.java
@@ -9,15 +9,36 @@ public class Fox {
     WATER
   }
 
+  public enum Food {
+    PIZZA,
+    SALAD
+  }
+
   private String name;
   private List<String> tricks;
-  private String food;
+  private Food food;
   private Drink drink;
 
-  public Fox(String name, List<String> tricks, String food, Drink drink) {
+  public Fox(String name, List<String> tricks, Food food, Drink drink) {
     this.name = name;
     this.tricks = tricks;
     this.food = food;
     this.drink = drink;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public List<String> getTricks() {
+    return tricks;
+  }
+
+  public Food getFood() {
+    return food;
+  }
+
+  public Drink getDrink() {
+    return drink;
   }
 }
